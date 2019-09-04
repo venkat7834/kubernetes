7872cf4502afc7a03e7243806767edc32336ec0d
venkat7834 <venkat7834@gmail.com>
2019-09-04 12:41:47 +0530

    Create unix_kops.sh

---
+++ Kubernetes/unix_kops.sh
@@ -0,0 +1,14 @@
+#kubernetes installation by kops in ubuntu
+hostnamectl set-hostname kube.example.com
+apt-get update -y
+apt-get install python3 python3-pip -y
+pip3 install awscli
+#aws configure
+#install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+#install kops
+curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+ chmod +x kops-linux-amd64
+ sudo mv kops-linux-amd64 /usr/local/bin/kops
